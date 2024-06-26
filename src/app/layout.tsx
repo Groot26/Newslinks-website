f3b76317ededb3b5c1ecc6b40b586cd4ae1ece26
Groot26 <saimorajkar123@gmail.com>
2024-06-26 23:18:07 +0530

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,41 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "NewsLinks",
   description: "NewsLinks the news app",
+  metadataBase: new URL('https://newslinks-website.vercel.app/'),
+  openGraph: {
+    title: 'NewsLinks',
+    description: 'The official Website of NewsLinks.',
+    url: 'https://newslinks-website.vercel.app/',
+    siteName: 'NewsLinks',
+    images: [
+      {
+        url: 'https://newslinks-website.vercel.app/assets/OG.png',
+        width: 800,
+        height: 600,
+        alt: 'NewsLinks',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'NewsLinks',
+    description: 'The official Website of NewsLinks.',
+    images: [
+      {
+        url: 'https://newslinks-website.vercel.app/assets/OG.png',
+        width: 800,
+        height: 600,
+        alt: 'NewsLinks',
+      },
+    ],
+  },
+  icons: {
+    icon: 'https://newslinks-website.vercel.app/app/favicon.ico',
+    shortcut: 'https://newslinks-website.vercel.app/app/favicon.ico',
+    apple: 'https://newslinks-website.vercel.app/app/favicon.ico',
+  },
 };
+
 
 export default function RootLayout({
   children,
